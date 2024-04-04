ea99057d9f172591ec1a3e5101c4993d7ab028d9
soramon0 <23435312+soramon0@users.noreply.github.com>
2024-04-04 23:11:25 +0000

    add prettierd, tailwindcss language server

--- init.lua
+++ init.lua
@@ -557,6 +557,7 @@ require('lazy').setup({
         gopls = {},
         pyright = {},
         rust_analyzer = {},
+        prettierd = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -595,6 +596,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'tailwindcss-language-server',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
