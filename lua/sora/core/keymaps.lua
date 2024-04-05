06d699b2f8d637b395393c4ad5926af2c6101db5
soramon0 <23435312+soramon0@users.noreply.github.com>
2024-04-05 21:32:56 +0000

    pass ui lazy config to lazy config setup

--- lua/sora/core/keymaps.lua
+++ lua/sora/core/keymaps.lua
@@ -50,3 +50,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
