ad5b4274a03b2bb833afc2f4091b4713a156d821
soramon0 <23435312+soramon0@users.noreply.github.com>
2024-11-22 17:06:18 +0100

    fix: which key config and typescript lsp

--- lua/sora/plugins/init.lua
+++ lua/sora/plugins/init.lua
@@ -30,29 +30,27 @@ return {
 
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
-
-      -- Document existing key chains
-      require('which-key').add {
-        { '<leader>c', group = '[C]ode' },
-        { '<leader>c_', hidden = true },
-        { '<leader>d', group = '[D]ocument' },
-        { '<leader>d_', hidden = true },
-        { '<leader>r', group = '[R]ename' },
-        { '<leader>r_', hidden = true },
-        { '<leader>s', group = '[S]earch' },
-        { '<leader>s_', hidden = true },
-        { '<leader>w', group = '[W]orkspace' },
-        { '<leader>w_', hidden = true },
-      }
-      -- ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      -- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      -- ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      -- ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      -- ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-    end,
+    event = 'VeryLazy',
+    opts = {},
+    keys = {
+      {
+        '<leader>?',
+        function()
+          require('which-key').show { global = false }
+        end,
+        desc = 'Buffer Local Keymaps (which-key)',
+      },
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>c_', hidden = true },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>d_', hidden = true },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>r_', hidden = true },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>s_', hidden = true },
+      { '<leader>w', group = '[W]orkspace' },
+      { '<leader>w_', hidden = true },
+    },
   },
 
   -- Highlight todo, notes, etc in comments
