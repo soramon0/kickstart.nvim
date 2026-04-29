211b8461f821368db2fe779e134e38c5291644fc
soramon0 <23435312+soramon0@users.noreply.github.com>
2026-04-29 15:06:38 +0100

    add gruvbox color

--- lua/sora/plugins/colorscheme.lua
+++ lua/sora/plugins/colorscheme.lua
@@ -1,4 +1,36 @@
 return {
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000, -- Make sure it loads first
+    config = function()
+      -- Default options:
+      require('gruvbox').setup {
+        terminal_colors = true,
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = true,
+          emphasis = true,
+          comments = true,
+          operators = false,
+          folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, highlighting...
+        contrast = 'hard', -- can be "soft", "medium" or "hard"
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
+      }
+      vim.cmd 'colorscheme gruvbox'
+    end,
+  },
   -- {
   --   'rebelot/kanagawa.nvim',
   --   init = function()
@@ -25,29 +57,29 @@ return {
   --     vim.cmd.colorscheme 'catppuccin-mocha'
   --   end,
   -- },
-  {
-    -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = 'transparent',
-        floats = 'transparent',
-      },
-    },
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
+  -- {
+  --   -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = 'transparent',
+  --       floats = 'transparent',
+  --     },
+  --   },
+  --   init = function()
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
 }
--- lua/sora/plugins/treesitter.lua
+++ lua/sora/plugins/treesitter.lua
@@ -18,6 +18,7 @@ return {
       'html',
       'lua',
       'markdown',
+      'make',
       'vim',
       'vimdoc',
       'json',
